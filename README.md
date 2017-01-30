f1fb21567011b05b14d1f29fe91fe77ef5ab5cdc
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-01-30 21:08:56 +0100

    post => get

--- README.md
+++ README.md
@@ -10,20 +10,30 @@ list of open games
 #### /game/*:gamenumber*
 game *:gamenumber* infos and state
 
-### POST :
-#### /game/*:gamenumber*
-creates a new game / joins an existing game
+#### /game/create
+creates a new game  
+returns : game id
 
-#### /game/*:gamenumber*/player/*:player*/square/*:squarenumber*
-play square *:squarenumber*, for player *:player* in game *:gamenumber*
-Returns :
-* 1 for ok,
-* -1 for not this player's turn
-* -2 for square not playable,
-* -3 for other error
+#### /game/join
+tries to join an open game  
+returns :
+* -1 no open game
+* { id: game id, player: X or O }
 
 #### /game/*:gamenumber*/player/*:player*/pass/
 Passes the turn
 
 #### /game/*:gamenumber*/player/*:player*/giveup/
 Gives up the game
+
+#### /game/*:gamenumber*/player/*:player*/square/*:squarenumber*
+Plays square *:squarenumber*, for player *:player*, in game *:gamenumber*  
+Returns :
+* 1 for ok,
+* -1 for not this player's turn
+* -2 for square not playable,
+* -3 for other error
+
+### POST :
+
+Nothing yet
--- server.js
+++ server.js
@@ -4,34 +4,32 @@ var app = express();
 app.get('/', function(req, res){
   res.setHeader('Content-Type', 'text/plain');
   res.end('/game/ /game/[game-number]/ /game/[game-number]/player/[player]/square/[square-number]  /game/[game-number]/player/[player]/pass/ /game/[game-number]/player/[player]/give-up');
-});
-
-app.get('/game', function(req, res){
+})
+  .get('/game', function(req, res){
   res.setHeader('Content-Type', 'application/json');
   // return a list of open games
   res.end('');
 })
-
-app.post('/game/:gamenumber', function(req, res){
+  .get('/game/:gamenumber', function(req, res){
   res.setHeader('Content-Type', 'application/json');
   // creates a new game at :gamenumber
   res.end('');
 })
-
-app.get('/game/:gamenumber', function(req, res){
+  .get('/game/:gamenumber', function(req, res){
   res.setHeader('Content-Type', 'application/json');
-  // returns the game state
-  res.end('');
-})
+  if (req.params.gamenumber > 0){
 
-app.post('/game/:gamenumber/player/:player/play/:squarenumber', function(req, res){
+  }
+  // returns the game state
+  res.end({ code: '-1', message: 'Game not found'});
+})
+  .get('/game/:gamenumber/player/:player/play/:squarenumber', function(req, res){
   res.setHeader('Content-Type', 'text/plain');
-  // play square :squarenumber, for player :player in game :gamenumber
+  // plays square :squarenumber, for player :player in game :gamenumber
   // return code state ?? 1 for ok, -1 for not this player's turn, -2 for square not playable, -3 for error ?
   res.end('1');
 })
-
-app.use(function(req, res, next){
+  .use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.send(404, 'Not found !');
 })
