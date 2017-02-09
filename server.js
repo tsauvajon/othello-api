86b4d7af368c42d99316eafd2d9e7ef54b5d5184
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-02-09 16:07:52 +0100

    Ajout CORS

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', router);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // log
+  console.log('CORS enabled');
+  next();
+});
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Not found !');
@@ -26,26 +33,27 @@ router.route('/games')
   Game.find(function(err, games) {
     if (err)
       res.send(err);
-      res.json(games);
+    res.json(games);
   });
 });
 
 router.route('/games/:name')
   .post(function(req, res){
-  res.setHeader('Content-Type', 'application/json');
-  // creates a new game
-  var game = new Game();
-  game.name = req.params.name;
-  game.squares = Array(64).fill(null);
-  game.xIsNext = false;
-  game.stepNumber = 0;
-  game.save(function(err) {
-    if (err)
-      res.status(503).json(err);
-    res.json(game);
+    res.setHeader('Content-Type', 'application/json');
+    // creates a new game
+    var game = new Game();
+    game.name = req.params.name;
+    game.squares = Array(64).fill(null);
+    game.xIsNext = false;
+    game.stepNumber = 0;
+    game.save(function(err) {
+      if (err)
+        res.status(503).json(err);
+      res.json(game);
+    });
   });
-});
-  router.route('/games/:gameid')
+
+router.route('/games/:gameid')
   .get(function(req, res){
     res.setHeader('Content-Type', 'application/json');
     Game.findById(req.params.gameid, function(err, game) {
@@ -81,9 +89,7 @@ router.route('/games/:name')
       res.json({ message: 'Deleted !' });
     });
   });
-  // returns the game state
-  // res.json({ code: 404, message: 'Game not found'});
-// });
+
 //   .get('/players/:player/play/:squarenumber', function(req, res){
 //   res.setHeader('Content-Type', 'text/plain');
 //   // plays square :squarenumber, for player :player in game :gameid
@@ -95,19 +101,11 @@ router.route('/games/:name')
 //     res.json('X');
 //   });
 
-router.use(function (req, res, next) {
-  // log
-  console.log('Smth happened');
-  next();
-});
-
 var server = app.listen(process.env.PORT || 8081, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("Othello API running on port %s", port)
 });
-
-
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017')
